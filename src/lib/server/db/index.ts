7ea243e72ed6bc5412d95a2165a3d0e1916376e6
iC4rds <dstaud2005@gmail.com>
2026-02-19 11:36:52 +0100

    fix

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,7 +5,7 @@ import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-if (!dev && !env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
+if (!env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
 
 const client = createClient({
 	url: env.DATABASE_URL,

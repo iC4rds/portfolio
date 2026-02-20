befc18d33245a7e645f4ed6cdd24c3d94fe5972f
iC4rds <dstaud2005@gmail.com>
2026-02-20 15:07:03 +0100

    update index

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,14 +1,17 @@
 import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
+import { createClient } from '@libsql/client/http';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-if (!env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
+// Prüfung nur, wenn wir nicht im Build sind
+if (process.env.NODE_ENV !== 'production' || !process.env.CF_PAGES) {
+  if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+  if (!env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
+}
 
 const client = createClient({
-	url: env.DATABASE_URL,
-	authToken: env.DATABASE_AUTH_TOKEN,
+  url: env.DATABASE_URL!,
+  authToken: env.DATABASE_AUTH_TOKEN!,
 });
 
 export const db = drizzle(client, { schema });
